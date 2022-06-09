fe8eefef689acb1250db4bc332471e1a1a42bec7
Jose Balado <JoseBalado@users.noreply.github.com>
2022-06-10 00:50:16 +0200

    Remove unneded JavaScript code.

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -2,49 +2,16 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-//Disable the send button until connection is established.
-document.getElementById("sendButton").disabled = true;
-
-/*
-connection.on("ReceiveMessage", function (user, message) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message}`;
-});
-*/
-
 connection.on("ReceiveMessage", function (user, message) {
     document.getElementById("percentageUsage").innerHTML = "CPU Percentage Usage: " + message;
 });
 
 connection.on("SendQueue", function (user, message) {
-    window.percentages = message;
-    var messageList = document.getElementById("messagesList");
-    messageList.innerHTML = "";
-    window.percentages.forEach(number => {
-        var li = document.createElement("li");
-        messageList.appendChild(li);
-        li.textContent = `${user} says ${number}`;
-    });
-
     drawPercentage(message);
-
 });
 
 connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
+    console.log("Connection started.");
 }).catch(function (err) {
     return console.error(err.toString());
-});
-
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error(err.toString());
-    });
-    event.preventDefault();
 });
